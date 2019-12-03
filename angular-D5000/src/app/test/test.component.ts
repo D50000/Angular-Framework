55d88786db66f79d7e78321f73457db326216620
Eddie Hsu <aoe102198@gmail.com>
2019-12-03 11:27:21 +0800

    initial the demo project
    Angular Structure

---
+++ angular-D5000/src/app/test/test.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import { Input } from '@angular/core';
+import { Output, EventEmitter } from '@angular/core';
+
+@Component({
+  selector: 'app-test',
+  templateUrl: './test.component.html',
+  styleUrls: ['./test.component.css']
+})
+export class TestComponent implements OnInit {
+  @Input() productx;
+  @Output() notify = new EventEmitter();
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
